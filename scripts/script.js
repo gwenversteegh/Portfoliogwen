ad7576a632526cccb3b5f7cebc81f49d409a1a3f
gwenversteegh <70900763+gwenversteegh@users.noreply.github.com>
2024-07-25 23:31:55 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,5 @@
 //Decleration
 var sidebar = document.querySelectorAll('section');
-var menuSlide = document.querySelector('.menu');
-var menuButton = document.querySelector('.hamburger');
-var kruisje = document.querySelector('.kruisje');
 
 function sectionInViewport(element) {
     "use strict";
@@ -35,20 +32,3 @@ document.addEventListener('scroll', function () {
 });
 
 
-function menuslider() {
-    "use strict";
-    menuSlide.classList.add('menuslide');
-}
-
-function menusliderback() {
-    "use strict";
-    menuSlide.classList.add('menuslideback');
-    menuSlide.classList.remove('menuslide');
-    setTimeout(function () {
-        menuSlide.classList.remove('menuslideback');
-    }, 1000);
-}
-
-//eventhandlers, bepalen wanneer de functie word aangeroepen
-menuButton.addEventListener('click', menuslider);
-kruisje.addEventListener('click', menusliderback);
